61cdec3d126c09670dfc612bf16055921b2c64ea
adarsh pathak <adarshpathak826@gmail.com>
2024-02-27 23:32:07 +0530

    side bar in progress

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,10 +1,49 @@
 import React from 'react';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { IconButton } from '@mui/material';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import GroupAddIcon from '@mui/icons-material/GroupAdd';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import NightlightIcon from '@mui/icons-material/Nightlight';
+import SearchIcon from '@mui/icons-material/Search';
+
 import "../Components/myStyles.css";
 
 
 const Sidebar = () => {
   return (
-    <div className='sidebar-container'>Sidebar</div>
+    <div className='sidebar-container'>
+        <div className='sb-header'>
+           <div>
+            <IconButton>
+            <AccountCircleIcon/>
+            </IconButton>
+           </div>
+
+            <div>
+            <IconButton>
+            <PersonAddIcon/>
+            </IconButton>
+
+            <IconButton>
+            <GroupAddIcon/>
+            </IconButton>
+
+            <IconButton>
+            <AddCircleIcon/>
+            </IconButton>
+
+            <IconButton>
+            <NightlightIcon/>
+            </IconButton>
+            </div>
+        </div>
+        <div className='sb-search'>
+            <SearchIcon/>
+            <input className='search-box' type='text' placeholder='Search'/>
+            </div>
+        <div className='sb-conversations'>Header</div>
+    </div>
   )
 }
 
